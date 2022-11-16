40e16112a8ec0d4ef5e480c0ded48e83e0dac5f7
Stanislav Huseinov <stashuseinov@gmail.com>
2022-11-16 13:12:42 +0200

    done project

---
+++ app/libs/libs.js
@@ -0,0 +1,12 @@
+//= jquery/jquery-3.4.1.min.js
+//= slick/slick.min.js
+//= wNumb/wNumb.min.js
+//= noui/nouislider.min.js
+//= fancybox/jquery.fancybox.min.js
+//= select2/select2.min.js
+//= mask/jquery.inputmask.min.js
+//= wow/wow.min.js
+//= autocomplete/autocomplete.min.js
+
+
+
